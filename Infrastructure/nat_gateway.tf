e6e7f7c5570107111d3071759c2ff3f95f952d82
sl11me <sl11me@outlook.com>
2024-11-21 03:40:14 -0500

    first 3tier Infra commit

---
+++ Infrastructure/nat_gateway.tf
@@ -0,0 +1,19 @@
+#####################
+#   NAT Gateway #
+#####################
+
+resource "aws_eip" "eip_nat" {
+
+  tags = {
+    Name = "eip1"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1" {
+  allocation_id = aws_eip.eip_nat.id
+  subnet_id     = aws_subnet.public-web-subnet-2.id
+
+  tags = {
+    "Name" = "nat1"
+  }
+}
