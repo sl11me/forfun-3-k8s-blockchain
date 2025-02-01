0d1e9d57538c2ae3a34e8bee76d09930c50e7001
sl11me <sl11me@outlook.com>
2025-02-01 04:44:06 -0500

    renom main dossier Sl11me + infos readme

---
+++ sl11me/ec2.tf
@@ -0,0 +1,56 @@
+########################
+###    Data source   ###
+########################
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+##############################
+#### EC2 instance Web Tier ###
+##############################
+
+resource "aws_instance" "PublicWebTemplate" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public-web-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
+  key_name               = file("3tier.pem")
+  user_data              = file("install-apache.sh")
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "web autoscaled instance"
+  }
+}
+
+##############################
+#### EC2 instance App Tier ###
+##############################
+
+resource "aws_instance" "private-app-template" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private-app-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.ssh-security-group.id]
+  key_name               = file("3tier.pem")
+
+  tags = {
+    Name = "app autoscaled instance"
+  }
+}
+
+
+
+
